8a8f276aad5cb9b77bbd241dfcd163467c9db204
phuclh@minerva.vn <hoangphuc98>
2021-01-18 13:14:05 +0700

    - connect poly google api

--- app/src/main/java/hphuc5453/demo_3d_model/MainActivity.kt
+++ app/src/main/java/hphuc5453/demo_3d_model/MainActivity.kt
@@ -1,11 +1,47 @@
 package hphuc5453.demo_3d_model
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.github.kittinunf.fuel.android.extension.responseJson
+import com.github.kittinunf.fuel.httpGet
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val key = "AIzaSyB8jJtGtVQaEvRySNtmMNo3lxtAUpdup_E"
+        const val baseURL = "https://poly.googleapis.com/v1"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listURL = "$baseURL/assets"
+        listURL.httpGet(listOf(
+                "category" to "animals",
+                "key" to key,
+                "format" to "OBJ"
+        )).responseJson { _, _, result ->
+            result.fold({
+                // Get assets array
+                val assets = it.obj().getJSONArray("assets")
+
+                // Loop through array
+                for(i in 0 until assets.length()) {
+                    // Get id and displayName
+                    val id = assets.getJSONObject(i).getString("name")
+                    val displayName =
+                            assets.getJSONObject(i).getString("displayName")
+
+                    // Print id and displayName
+                    Log.d("POLY", "(ID: $id) -- (NAME: $displayName)")
+                }
+            }, {
+                // In case of an error
+                Log.e("POLY", "An error occurred")
+            })
+            // read JSON data
+        }
     }
 }
